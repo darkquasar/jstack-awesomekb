dc87409e0f2d2487592df90fa440eb07705ffe11
darkquasar <darkquasar7@gmail.com>
2018-11-12 22:19:00 -0800

    -

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'DQ-AwesomeKB'
-copyright = '2018, Diego Perez (@darkquasar)'
+copyright = '2018, Diego Perez (@darkquassar)'
 author = 'Diego Perez (@darkquasar)'
 
 # The version info for the project you're documenting, acts as replacement for
